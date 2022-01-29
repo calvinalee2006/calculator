935d076aa9258464a52b4cf3fd5f24e00b1d1183
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-01-29 11:15:05 -0600

    finally got numbers!

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-const calcBtns = document.getElementById('calc-btns').children;
+const calcBtns = document.querySelectorAll('.calc-btns');
+
 const display = document.getElementById('display-screen');
+const numOne = document.getElementById('one');
+
+//numOne.addEventListener('click', (e) => {
+//console.log(e.target.textContent);
+//display.value = e.target.textContent;
+//});
+
+calcBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        console.log(e.target.textContent);
+        display.value += e.target.textContent;
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+//all numbers together
+//all operators together
+//Use a for loop
+
 
 
 
