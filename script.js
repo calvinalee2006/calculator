201f3d09567478e528834bece027476c4ce71cfb
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-01-30 09:43:45 -0600

    changes

--- script.js
+++ script.js
@@ -1,19 +1,77 @@
 const calcBtns = document.querySelectorAll('.calc-btns');
-
+const calcNumber = document.querySelectorAll('#number');
+const calcOperator = document.querySelectorAll('#operator')
+const enterSum = document.querySelector('#enter');
 const display = document.getElementById('display-screen');
-const numOne = document.getElementById('one');
+
+
+//const numOne = document.getElementById('one');
 
 //numOne.addEventListener('click', (e) => {
 //console.log(e.target.textContent);
 //display.value = e.target.textContent;
 //});
 
-calcBtns.forEach(btn => {
+//calcBtns.forEach(btn => {
+//btn.addEventListener('click', (e) => {
+//console.log(e.target.textContent);
+//display.value += e.target.textContent;
+//})
+//})
+
+let firstOperand = "";
+let secondOperand = "";
+let operator = ""
+
+calcNumber.forEach(btn => {
     btn.addEventListener('click', (e) => {
         console.log(e.target.textContent);
         display.value += e.target.textContent;
     })
 })
+
+calcOperator.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        console.log(e.target.textContent);
+        display.value += e.target.textContent;
+
+    })
+})
+
+function solveProblem() {
+    enterSum.addEventListener('click', () => {
+        if (operator === "+") {
+            return display.value = firstOperand + secondOperand;
+        }
+        if (operator === "-") {
+            return display.value = firstOperand - secondOperand;
+        }
+        if (operator === "X") {
+            return display.value = firstOperand * secondOperand;
+        }
+        if (operator === "/") {
+            return display.value = firstOperand / secondOperand;
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
