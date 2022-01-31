48e66db7f2cb61df66e28cafa07300a59669b5aa
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-01-30 20:15:11 -0600

    try to have multiple numbers

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const calcBtns = document.querySelectorAll('.calc-btns');
-const calcNumber = document.querySelectorAll('#number');
-const calcOperator = document.querySelectorAll('#operator')
-const enterSum = document.querySelector('#enter');
+const calcNumber = document.querySelectorAll('.number');
+const calcOperator = document.querySelectorAll('.operator')
+const enterSum = document.querySelector('.enter');
 const display = document.getElementById('display-screen');
 
 
@@ -18,84 +18,82 @@ const display = document.getElementById('display-screen');
 //display.value += e.target.textContent;
 //})
 //})
-
+let firstOperandStored = true;
 let firstOperand = "";
 let secondOperand = "";
-let operator = ""
+let operator = "";
 
-calcNumber.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        console.log(e.target.textContent);
-        display.value += e.target.textContent;
-    })
+
+document.addEventListener('click', function (e) {
+    if (e.target.className === "number") {
+        if (firstOperandStored === true) {
+            display.value = "";
+            display.value = e.target.textContent;
+        }
+        if (firstOperandStored === false) {
+            console.log(e.target.textContent);
+            display.value = e.target.textContent;
+        }
+
+    }
 })
 
-calcOperator.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        console.log(e.target.textContent);
-        display.value += e.target.textContent;
 
-    })
+document.addEventListener('click', function (e) {
+    if (e.target.className === "operator") {
+        firstOperand = display.value;
+        operator = e.target.textContent;
+        console.log(firstOperand)
+        //console.log(e.target.textContent);
+        firstOperandStored = true;
+    }
 })
+
+document.addEventListener('click', function (e) {
+    if (e.target.className === "decimal") {
+        decimal = display.value;
+        console.log(e.target.textContent);
+        display.value = e.target.textContent;
+    }
+})
+
+document.addEventListener('click', function (e) {
+    if (e.target.className === "enter") {
+        solveProblem();
+    }
+})
+
+
+
+// calcOperator.forEach(btn => {
+//     btn.addEventListener('click', (e) => {
+//         console.log(e.target.textContent);
+//         display.value += e.target.textContent;
+
+//     })
+// })
 
 function solveProblem() {
-    enterSum.addEventListener('click', () => {
-        if (operator === "+") {
-            return display.value = firstOperand + secondOperand;
-        }
-        if (operator === "-") {
-            return display.value = firstOperand - secondOperand;
-        }
-        if (operator === "X") {
-            return display.value = firstOperand * secondOperand;
-        }
-        if (operator === "/") {
-            return display.value = firstOperand / secondOperand;
-        }
-    })
+    if (operator == "+") {
+        secondOperand = display.value;
+        display.value = +firstOperand + +secondOperand; //+ is making sure numbers are numbers and they are not concatenating.
+    }
+    if (operator === "-") {
+        secondOperand = display.value;
+        display.value = +firstOperand - +secondOperand;
+    }
+    if (operator === "X") {
+        secondOperand = display.value;
+        display.value = +firstOperand * +secondOperand;
+    }
+    if (operator === "/") {
+        secondOperand = display.value;
+        display.value = +firstOperand / +secondOperand;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 //all numbers together
 //all operators together
 //Use a for loop
-
-
-
-
-
-
-
-
-
-
-
-
