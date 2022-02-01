b784aea8912b6a9b31826954606c6d1b9c39b3ef
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-02-01 07:28:48 -0600

    back to the drawing bord

--- script.js
+++ script.js
@@ -1,120 +1,26 @@
-const calcBtns = document.querySelectorAll('.calc-btns');
-const calcNumber = document.querySelectorAll('.number');
-const calcOperator = document.querySelectorAll('.operator')
-const enterSum = document.querySelector('.enter');
-const display = document.getElementById('display-screen');
+const displayScreen = document.getElementById('display-screen');
+const numberBtns = document.querySelectorAll(".number");
+const calcPad = document.getElementById("calc-btns");
 
-
-//const numOne = document.getElementById('one');
-
-//numOne.addEventListener('click', (e) => {
-//console.log(e.target.textContent);
-//display.value = e.target.textContent;
-//});
-
-//calcBtns.forEach(btn => {
-//btn.addEventListener('click', (e) => {
-//console.log(e.target.textContent);
-//display.value += e.target.textContent;
-//})
-//})
-let firstOperandStored = true;
 let firstOperand = "";
 let secondOperand = "";
-let operator = "";
+let operator = " ";
 
+numberBtns.forEach(number => {
+    number.addEventListener('click', event => {
+        const digit = event.target
+        const firstValue = digit.textContent
+        const displayValue = displayValue.textContent
 
-document.addEventListener('click', function (e) {
-    //for loop? or forEach? hmmm......
-    if (e.target.className === "number") {
-        if (firstOperandStored === true) {
-            display.value = "";
-            display.value = e.target.textContent;
+        //now time to get multiple digits
+
+        if (displayScreen === '0') {
+            displayScreen.textContent = firstValue
+        } else {
+            displayScreen.textContent = `${digit} ${displayValue}`;
+            console.log(numberBtns)
         }
-        if (firstOperandStored === false) {
-            console.log(e.target.textContent);
-            display.value = e.target.textContent;
-        }
-
-    }
+    })
 })
 
 
-document.addEventListener('click', function (e) {
-    if (e.target.className === "operator") {
-        firstOperand = display.value;
-        operator = e.target.textContent;
-        console.log(firstOperand)
-        //console.log(e.target.textContent);
-        firstOperandStored = true;
-    }
-})
-
-document.addEventListener('click', function (e) {
-    if (e.target.className === "L-para") {
-        decimal = display.value;
-        console.log(e.target.textContent);
-        display.value = e.target.textContent;
-    }
-})
-
-document.addEventListener('click', function (e) {
-    if (e.target.className === "R-para") {
-        decimal = display.value;
-        console.log(e.target.textContent);
-        display.value = e.target.textContent;
-    }
-})
-
-document.addEventListener('click', function (e) {
-    if (e.target.className === "clear") {
-        //Enter functionality for clear button
-    }
-})
-
-document.addEventListener('click', function (e) {
-    if (e.target.className === "percent") {
-        //Enter functionality for modulo
-    }
-})
-
-document.addEventListener('click', function (e) {
-    if (e.target.className === "enter") {
-        solveProblem();
-    }
-})
-
-
-
-// calcOperator.forEach(btn => {
-//     btn.addEventListener('click', (e) => {
-//         console.log(e.target.textContent);
-//         display.value += e.target.textContent;
-
-//     })
-// })
-
-function solveProblem() {
-    if (operator == "+") {
-        secondOperand = display.value;
-        display.value = +firstOperand + +secondOperand; //+ is making sure numbers are numbers and they are not concatenating.
-    }
-    if (operator === "-") {
-        secondOperand = display.value;
-        display.value = +firstOperand - +secondOperand;
-    }
-    if (operator === "X") {
-        secondOperand = display.value;
-        display.value = +firstOperand * +secondOperand;
-    }
-    if (operator === "/") {
-        secondOperand = display.value;
-        display.value = +firstOperand / +secondOperand;
-    }
-}
-
-
-
-//all numbers together
-//all operators together
-//Use a for loop
