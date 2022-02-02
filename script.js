ffdae178d47eb61b45786c7ba0c2a97e54168c03
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-02-02 07:13:36 -0600

    made huge progress

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
 const displayScreen = document.getElementById('display-screen');
-const numberBtns = document.querySelectorAll(".number");
+const numberBtns = document.querySelectorAll(".number")
+const operatorBtns = document.querySelectorAll(".operator")
 const calcPad = document.getElementById("calc-btns");
 
 let firstOperand = "";
 let secondOperand = "";
 let operator = " ";
 
+//For my numbers on my calculator
 numberBtns.forEach(number => {
     number.addEventListener('click', event => {
-        const digit = event.target
-        const firstValue = digit.textContent
-        const displayValue = displayValue.textContent
-
-        //now time to get multiple digits
-
-        if (displayScreen === '0') {
-            displayScreen.textContent = firstValue
+        //console.log(event.target.textContent);
+        displayScreen.value += event.target.textContent
+        if (operator === "") {
+            firstOpernd += event.target.textContent;
+            console.log(firstOperand)
         } else {
-            displayScreen.textContent = `${digit} ${displayValue}`;
-            console.log(numberBtns)
-        }
+            secondOperand += event.target.textContent;
+            console.log(secondOperand)
+        };
+    });
+})
+
+//For my Operators on my calculator 
+operatorBtns.forEach(symbol => {
+    symbol.addEventListener('click', event => {
+        console.log(event.target.textContent);
+        displayScreen.value += event.target.textContent
     })
 })
+
+//Enter Button
+document.addEventListener('click', function (e) {
+    if (e.target.className === "enter") {
+        solveProblem();
+    }
+})
+
+//Math symbols function
+function solveProblem() {
+    if (operator == "+") {
+        return `${firstOperand} + ${secondOperand}`
+    }
+    if (operator === "-") {
+        secondOperand = display.value;
+        display.value = +firstOperand - +secondOperand;
+    }
+    if (operator === "X") {
+        secondOperand = display.value;
+        display.value = +firstOperand * +secondOperand;
+    }
+    if (operator === "/") {
+        secondOperand = display.value;
+        display.value = +firstOperand / +secondOperand;
+    }
+}
+
+
 
 
